6273c61b0baab2001bd438a7ae3f0cfa4506d5ed
Oscar Andersson <oscar.andersson@nordicnest.com>
2026-08-07 12:53:53 +0200

    add header comments to keymaps

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,18 +1,23 @@
 local map = vim.keymap.set
 
+-- Search
 map('n', '<Esc>', '<cmd>nohlsearch<CR>', { desc = 'Clear search highlight' })
 
+-- Windows
 map('n', '<C-h>', '<C-w>h', { desc = 'Go to left window' })
 map('n', '<C-j>', '<C-w>j', { desc = 'Go to lower window' })
 map('n', '<C-k>', '<C-w>k', { desc = 'Go to upper window' })
 map('n', '<C-l>', '<C-w>l', { desc = 'Go to right window' })
 
+-- Editing
 map('v', 'J', ":m '>+1<CR>gv=gv", { desc = 'Move selection down' })
 map('v', 'K', ":m '<-2<CR>gv=gv", { desc = 'Move selection up' })
 
+-- Diagnostics
 map('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic' })
 map('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Diagnostics to loclist' })
 
+-- LSP
 map('n', '<leader>cf', function() vim.lsp.buf.format({ async = true }) end, { desc = 'Format' })
 map('n', '<leader>ca', vim.lsp.buf.code_action, { desc = 'Code action' })
 map('n', '<leader>co', function()
@@ -22,4 +27,5 @@ map('n', '<leader>co', function()
   })
 end, { desc = 'Organize imports' })
 
+-- Neovim
 map('n', '<leader>wr', '<cmd>restart<CR>', { desc = 'Reload Neovim' })
