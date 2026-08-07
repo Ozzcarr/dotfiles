b2d9fa69e18f21cb7dc8961d07892d7609113db4
Oscar Andersson <oscar.andersson@nordicnest.com>
2026-08-07 11:12:27 +0200

    add lazydocker

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -25,10 +25,11 @@ require('snacks').setup({
 })
 
 local map = vim.keymap.set
+map('n', '<leader>dd', function() Snacks.terminal({ 'lazydocker' }) end, { desc = 'Lazydocker' })
+map('n', '<leader>fb', function() Snacks.picker.buffers() end, { desc = 'Find buffers' })
 map('n', '<leader>fe', function() Snacks.explorer() end, { desc = 'Explorer' })
 map('n', '<leader>ff', function() Snacks.picker.files() end, { desc = 'Find files' })
 map('n', '<leader>fg', function() Snacks.picker.grep() end, { desc = 'Live grep' })
-map('n', '<leader>fb', function() Snacks.picker.buffers() end, { desc = 'Find buffers' })
 map('n', '<leader>fr', function() Snacks.picker.recent() end, { desc = 'Recent files' })
 map('n', '<leader>gg', function() Snacks.lazygit() end, { desc = 'Lazygit' })
 map('n', '<leader>gl', function() Snacks.lazygit.log_file() end, { desc = 'Lazygit Log (cwd)' })
