cdadce00c5833be5b959b77dc895f3b7fb31cd66
Oscar Andersson <oscar.andersson@nordicnest.com>
2026-08-07 14:10:21 +0200

    include hidden files as default for files and grep

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -6,6 +6,8 @@ require('snacks').setup({
   picker = {
     sources = {
       explorer = { hidden = true },
+      files = { hidden = true },
+      grep = { hidden = true },
     },
   },
   dashboard = {
