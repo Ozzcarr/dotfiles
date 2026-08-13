ca187377735b327934a93b205e10e76545c6dedf
Oscar Andersson <anderssonoscar03@gmail.com>
2026-08-13 23:02:36 +0200

    add typescript support

--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -6,7 +6,11 @@ require('mason-registry').refresh(function()
   if not require('mason-registry').is_installed('lua-language-server') then
     require('mason-registry').get_package('lua-language-server'):install()
   end
+  if not require('mason-registry').is_installed('typescript-language-server') then
+    require('mason-registry').get_package('typescript-language-server'):install()
+  end
 end)
 
 vim.lsp.enable('lua_ls')
 vim.lsp.enable('nixd')
+vim.lsp.enable('ts_ls')
--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -5,18 +5,28 @@ vim.pack.add({
 local parsers = {
   'lua',
   'nix',
+  'typescript',
+  'tsx',
 }
 
 require('nvim-treesitter').install(parsers)
 
-local filetypes = {}
-for _, parser in ipairs(parsers) do
-  vim.list_extend(filetypes, vim.treesitter.language.get_filetypes(parser))
-end
+vim.schedule(function()
+  local filetypes = {}
+  for _, parser in ipairs(parsers) do
+    vim.list_extend(filetypes, vim.treesitter.language.get_filetypes(parser))
+  end
 
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = filetypes,
-  callback = function()
-    vim.treesitter.start()
-  end,
-})
+  vim.api.nvim_create_autocmd('FileType', {
+    pattern = filetypes,
+    callback = function()
+      vim.treesitter.start()
+    end,
+  })
+
+  for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+    if vim.tbl_contains(filetypes, vim.bo[buf].filetype) then
+      vim.treesitter.start(buf)
+    end
+  end
+end)
