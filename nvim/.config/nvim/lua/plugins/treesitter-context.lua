63adf19b9d3a3942d1465a02dac2647234741549
Oscar Andersson <anderssonoscar03@gmail.com>
2026-08-29 00:01:15 +0200

    remove treesitter context separator

--- nvim/.config/nvim/lua/plugins/treesitter-context.lua
+++ nvim/.config/nvim/lua/plugins/treesitter-context.lua
@@ -6,7 +6,6 @@ require('treesitter-context').setup({
   max_lines = 8,
   multiline_threshold = 3,
   trim_scope = 'inner',
-  separator = '─',
 })
 
 vim.keymap.set('n', '[c', function() require('treesitter-context').go_to_context(vim.v.count1) end,
